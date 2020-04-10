761f593f1f626fa6444499f4f1581d7358660957
jmsmf28 <jmsmf2011@gmail.com>
2020-04-10 19:45:13 +0100

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-    //mongoURI:'mongodb://localhost:27017/auth_user'
-    mongoURI:'mongodb+srv://jorge:Centaurus1C@cluster0-olb7y.mongodb.net/test?retryWrites=true&w=majority'
-}
